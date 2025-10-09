7be49848f9d2b1c615926f46ad32ebe529d454f5
KB1537 <Kylebenjamin437@gmail.com>
2025-10-09 19:41:01 +0100

    add let feature and disable game feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,13 @@ const lowGuess = document.getElementById('low-guess');
 const highGuess = document.getElementById('high-guess');
 const correctScoreEl = document.getElementById('score');
 const incorrectScoreEl = document.getElementById('Incorrect-score');
+
+let targetNumber = 0;
+let maxNumber = 0;
+let correctScore = 0;
+let incorrectScore = 0;
+
+// Disable input and submit until level is picked
+answerBox.disabled = true;
+submitButton.disabled = true;
+
