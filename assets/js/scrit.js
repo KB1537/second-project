ac4fd2da9610380b55a72468fd80307bfb664b2b
KB1537 <Kylebenjamin437@gmail.com>
2025-10-09 19:18:02 +0100

    add margin to box in css, define const in js

--- assets/js/scrit.js
+++ assets/js/scrit.js
@@ -1,1 +1,8 @@
-console.log('confirm')
+// Select elements
+const levelButtons = document.querySelectorAll('.controls button[data-type]');
+const submitButton = document.querySelector('.btn--grey');
+const answerBox = document.getElementById('answer-box');
+const lowGuess = document.getElementById('low-guess');
+const highGuess = document.getElementById('high-guess');
+const correctScoreEl = document.getElementById('score');
+const incorrectScoreEl = document.getElementById('Incorrect-score');
